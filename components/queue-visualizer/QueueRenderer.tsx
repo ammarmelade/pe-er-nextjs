b5338c52e90d3a3b91a7f8432d6cc79a545fda57
ammarmelade <ammarmelade@gmail.com>
2024-04-22 09:12:35 +0700

    Fix minor styling issue in Tailwind

--- components/queue-visualizer/QueueRenderer.tsx
+++ components/queue-visualizer/QueueRenderer.tsx
@@ -12,21 +12,21 @@ const QueueRenderer = () => {
         <>
             <Col className="gutter-row border-r-4 border-gray-500" span={4}>
                 <Space direction="vertical" size={"small"} style={{ display: 'flex' }}>
-                    <Card hoverable={true} className="border-4 border-black h-24 w-36 justify-center align-center"><p className="text-center font-bold">Cashier #1</p></Card>
+                    <Card hoverable={true} className="border-4 border-black h-24 w-36 justify-center align-center mx-auto"><p className="text-center font-bold">Cashier #1</p></Card>
                     {customers[0] && <CustomerRenderer customersArray={customers[0]}/>}
                 </Space>
             </Col>
 
             <Col className="gutter-row border-r-4 border-gray-500" span={4}>
                 <Space direction="vertical" size={"small"} style={{ display: 'flex' }}>
-                    <Card hoverable={true} className="border-4 border-black h-24 w-36 justify-center align-center"><p className="text-center font-bold">Cashier #2</p></Card>
+                    <Card hoverable={true} className="border-4 border-black h-24 w-36 justify-center align-center mx-auto"><p className="text-center font-bold">Cashier #2</p></Card>
                     {customers[1] && <CustomerRenderer customersArray={customers[1]}/>}
                 </Space>
             </Col>
 
             <Col className="gutter-row" span={4}>
                 <Space direction="vertical" size={"small"} style={{ display: 'flex' }}>
-                    <Card hoverable={true} className="border-4 border-black h-24 w-36 justify-center align-center"><p className="text-center font-bold">Cashier #3</p></Card>
+                    <Card hoverable={true} className="border-4 border-black h-24 w-36 justify-center align-center mx-auto"><p className="text-center font-bold">Cashier #3</p></Card>
                     {customers[2] && <CustomerRenderer customersArray={customers[2]}/>}
                 </Space>
             </Col>
